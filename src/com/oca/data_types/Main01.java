e04f36a56be60a640a5599f9eedb69d9e8d9d8d4
lepirus <lepirus@gmail.com>
2023-12-18 18:23:06 +0100

    Data types - Default values of uninitialized primitives

--- src/com/oca/data_types/Main01.java
+++ src/com/oca/data_types/Main01.java
@@ -1,10 +1,17 @@
 package com.oca.data_types;
 
 public class Main01 {
+
+    static boolean b;
+    static int[] ia = new int[1];
+    static char ch;
+    static boolean[] ba = new boolean[1];
+
     public static void main(String[] args) {
         System.out.println("Hello OCA tests!");
 
         exercise01();
+        exercise02();
 
         System.out.println("End OCA tests!");
     }
@@ -15,6 +22,15 @@ public class Main01 {
 //        System.out.println(new Boolean()));   // Error
         System.out.println(new Boolean("true"));
         System.out.println(new Boolean("trUE"));
+    }
+
+    private static void exercise02() {
+        boolean x = false;
+        if (b) {
+            x = (ch == ia[ch]);
+        } else x = (ba[ch] = b);
+        System.out.println(x + " " + ba[ch]);
+
     }
 
 }
