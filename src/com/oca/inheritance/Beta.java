8f9f6e0539ef03a7535cb1268ff62bcce9364325
lepirus <lepirus@gmail.com>
2023-12-19 13:10:27 +0100

    Inheritance

---
+++ src/com/oca/inheritance/Beta.java
@@ -0,0 +1,38 @@
+package com.oca.inheritance;
+
+class Baap {
+    public int h = 4;
+
+    public int getH() {
+        System.out.println("Baap " + h);
+        return h;
+    }
+}
+
+public class Beta extends Baap {
+
+
+    public int h = 44;
+
+    public int getH() {
+        System.out.println("Beta " + h);
+        return h;
+    }
+
+    /*
+    Always remember: Instance methods are overridden and variables are hidden. Which method is invoked depends on the class of the actual object, while which field is accessed depends on the class of the variable.
+    Here, b refers to an object of class Beta so b.getH() will always call the overridden (subclass's method). However, the type of reference of b is Baap. so b.h will always refer to Baap's h. Further, inside Beta's getH(), Beta's h will be accessed instead of Baap's h because you are accessing this.h ('this' is implicit) and the type of this is Beta.
+     */
+
+    public static void main(String[] args) {
+        exercise01();
+    }
+
+    private static void exercise01() {
+        Baap b = new Beta();
+
+        System.out.println(b.h + " " + b.getH());
+        Beta bb = (Beta) b;
+        System.out.println(bb.h + " " + bb.getH());
+    }
+}
