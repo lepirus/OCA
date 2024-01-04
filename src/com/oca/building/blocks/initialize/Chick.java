d97dfe3215cbea4b7df6bc018be73bf8a7ab720f
lepirus <lepirus@gmail.com>
2024-01-04 11:37:28 +0100

    Java Building Blocks - Creating Objects - Order of Initialization

---
+++ src/com/oca/building/blocks/initialize/Chick.java
@@ -0,0 +1,20 @@
+package com.oca.building.blocks.initialize;
+
+public class Chick {
+
+    private String name = "Fluffy";
+
+    public Chick() {
+        name = "Tiny";
+        System.out.println("setting constructor. Name: " + name);
+    }
+
+    {
+        System.out.println("setting field name: " + name);
+    }
+
+    public static void main(String[] args) {
+        Chick chick = new Chick();
+        System.out.println(chick.name);
+    }
+}
